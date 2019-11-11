f63b051b6246e9a464a39e777a2ab2446c27078b
Jaanek Kerma <Jaanek.Kerma@yh.nackademin.se>
2019-11-11 11:16:31 +0100

    Server multi user

--- src/Server/Server.java
+++ src/Server/Server.java
@@ -1,7 +1,6 @@
 package Server;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
@@ -22,7 +21,7 @@ public class Server extends Thread{
                 BufferedReader in = new BufferedReader(
                         new InputStreamReader(clientSocket.getInputStream()))
         ) {
-            String inputLine, outputLine;
+            String inputLine;
 
             ServerProtocol protocol = new ServerProtocol();
             out.println(protocol.processInput(null));
