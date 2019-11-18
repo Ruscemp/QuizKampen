38e5794d1d51a5d51ba07b8ac61b2659f90f704c
ValleTSF <valletsf@gmail.com>
2019-11-18 11:36:27 +0100

    Fixed two clients connecting to one server. Added turns, server only receives strings from client when its their turn.

---
+++ src/Server/ClientHandler.java
@@ -0,0 +1,79 @@
+package Server;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.net.SocketException;
+
+public class ClientHandler extends Thread {
+    private Socket player;
+    Question questionFromServer;
+    boolean yourTurn = false;
+
+
+    ClientHandler(Socket player) {
+        this.player = player;
+    }
+
+    @Override
+    public void run() {
+        try (
+                BufferedReader in = new BufferedReader(
+                        new InputStreamReader(player.getInputStream()));
+                ObjectOutputStream out = new ObjectOutputStream(player.getOutputStream());
+        ) {
+            String inputLine = "Placeholder";
+            Question question = new Question("Vad heter Englands huvudstad?", "Stockholm", "Birmingham", "Seoul", "London", Categories.GEOGRAPHY);
+
+            try {
+                while (true) {
+                    while (yourTurn) {
+                        Question questionToSend = questionFromServer;
+                        out.writeObject(question);
+
+                        inputLine = in.readLine();
+                        System.out.println(inputLine);
+                        if (inputLine.equalsIgnoreCase(question.correctAnswer)) {
+                            System.out.println("Win");
+                        } else {
+                            System.out.println("EHHH wrong, now its player 2s turn");
+                        }
+                        // out2.println(questions.getQuestion());
+                        //out.println(protocol.processInput(inputLine));
+                    }
+                }
+            } catch (SocketException e) {
+                e.printStackTrace();
+                System.out.println("Socket Exception: " + e.getMessage());
+
+            } catch (Exception e) {
+                System.out.println("Exception: " + e.getMessage());
+                e.printStackTrace();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("IOException: " + e.getMessage());
+        }
+
+    }
+
+    public void receiveQuestionFromServer(Question question) {
+        questionFromServer = question;
+    }
+
+    public void sendMessageToClient(String message){
+
+    }
+
+    public void setTurnTrue(){
+        yourTurn = true;
+    }
+
+    public void setTurnFalse(){
+        yourTurn = false;
+    }
+}
+
